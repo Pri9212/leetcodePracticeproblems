66062347cd46d5a360d54f88b6f2d82eaac22869
Pri9212 <yadavpriyanshu6528@gmail.com>
2026-07-30 18:26:18 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.73%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+    int n=word.size() ;
+    int ans=0;
+    if(n<=8) {
+        ans=n;
+    }
+    else if(n>8 && n<=16){
+    ans=8+(n-8)*2; 
+    }
+    else if(n>16&&n<=24){
+     ans=24+(n-16)*3;
+    }
+    else{
+        ans=48+4*(n-24);
+    }
+    return ans;
+    }
+
+};
