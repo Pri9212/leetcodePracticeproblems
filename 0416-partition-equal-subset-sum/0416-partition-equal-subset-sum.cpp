04d99d2d7d835365cdd886cf4bd009afdc97d06f
Pri9212 <yadavpriyanshu6528@gmail.com>
2026-08-18 19:41:06 +0530

    Time: 147 ms (39.96%), Space: 111.3 MB (18.36%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+bool solveusingmem(int target,vector<int>&nums){
+    int n=nums.size();
+ vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,0));
+for(int i=0;i<nums.size();i++){
+    dp[i][0]=1;
+}
+
+
+for( int index=n-1;index>=0;index--){
+    for(int t=1;t<=target;t++){
+        bool include=0;
+        if(t-nums[index]>=0)
+         include=dp[index+1][t-nums[index]];
+bool exclude=dp[index+1][t];
+dp[index][t]=(include || exclude);
+
+    }
+}
+
+return dp[0][target];
+}
+    bool canPartition(vector<int>& nums) {
+    
+      int sum=0;
+      for(int i=0;i<nums.size();i++) {
+        sum+=nums[i];
+
+      } 
+      if(sum%2!=0) return 0;
+      int target=sum/2;
+      int index=0;
+     
+      bool ans=solveusingmem(target,nums);
+      return ans;
+    }
+};
